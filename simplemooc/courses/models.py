f44407fa1eb530c41c8a8ed079f67284d63c3d85
FagnerSI <fagner314@gmail.com>
2018-08-09 12:21:41 -0300

    [:construction:] Em Construção

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -77,3 +77,38 @@ class Enrollment(models.Model):
         verbose_name = 'Inscrição'
         verbose_name_plural = 'Inscrições'
         unique_together = (('user','course'),)
+
+
+class Announcement(models.Model):
+
+    course = models.ForeignKey(Course, verbose_name='Curso', related_name='announcements')
+    title = models.CharField('Titulo', max_length=100)
+    content = models.TextField('Conteúdo')
+
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Anúncio'
+        verbose_name_plural = 'Anúncios'
+        ordering = ['-created_at']
+
+class Comment(models.Model):
+
+    announcement = models.ForeignKey(
+            Announcement, verbose_name='Anúncio', related_name='comments'
+        )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='usuário')
+    comment = models.TextField('Comentário')
+    
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Comentário'
+        verbose_name_plural = 'Comentários'
+
+
--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -83,6 +83,7 @@ def announcements(request, slug):
             return redirect('accounts:dashboard')
     template = 'courses/announcements.html'
     context = {
-        'course': course
+        'course': course,
+        'announcements': course.announcements.all()
     }
     return render(request, template, context)
